d102609c83e23aa62132ab1ab6ed28085ec5307b
Rohit Pravin Pandit <rohit.pandit26.rp@gmail.com>
2020-07-11 21:07:27 +0530

    Add files via upload

---
+++ Product price prediction.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 11 18:42:41 2020
+
+@author: pandit
+"""
+
+#import libraries 
+import pandas as pd  # Import Pandas for data manipulation using dataframes
+import numpy as np # Import Numpy for data statistical analysis
+import matplotlib.pyplot as plt # Import matplotlib for data visualisation
+import random #random numbers
+import seaborn as sns #data visualisation
+from matplotlib import pyplot as plt
+from matplotlib.dates import MonthLocator, num2date
+from matplotlib.ticker import FuncFormatter
+from fbprophet import Prophet
+# dataframes creation for both training and testing datasets
+avocado_df = pd.read_csv("avocado.csv")
+# Let's view the head of the training dataset
+avocado_df.head()
+# Let's view the last elements in the training dataset
+avocado_df.tail(10)
+avocado_df.describe()
+avocado_df.info()
+avocado_df.isnull().sum()
+#sort dates
+avocado_df = avocado_df.sort_values("Date")
+# Plot date and average price
+plt.figure(figsize=(10,10))
+plt.plot(avocado_df["Date"],avocado_df["AveragePrice"])
+# Plot distribution of the average price
+plt.figure(figsize=(10,6))
+sns.distplot(avocado_df["AveragePrice"],color='b')
+# Plot a violin plot of the average price vs. avocado type
+sns.violinplot(y="AveragePrice",x= "type",data=avocado_df)
+# Bar Chart to indicate the number of regions
+
+sns.set(font_scale=0.7)
+plt.figure(figsize=[25,12])
+sns.countplot(x = 'region', data = avocado_df)
+plt.xticks(rotation = 45)
+# Bar Chart to indicate the count in every year
+sns.set(font_scale=1.5)
+plt.figure(figsize=[25,12])
+sns.countplot(x = 'year', data = avocado_df)
+plt.xticks(rotation = 45)
+ # plot the avocado prices vs. regions for conventional avocados
+conventional = sns.catplot("AveragePrice","region",data=avocado_df[avocado_df['type']=='conventional'],
+hue="year",
+height = 20)
+  # plot the avocado prices vs. regions for organic avocados
+organic = sns.catplot("AveragePrice","region",data=avocado_df[avocado_df['type']=='organic'],hue='year',height=20)
+avocado_df
+avocado_prophet_df = avocado_df[['Date','AveragePrice']]
+avocado_prophet_df
+avocado_prophet_df = avocado_prophet_df.rename(columns={'Date':'ds','AveragePrice':'y'})
+avocado_prophet_df
+model1 = Prophet()
+model1.fit(avocado_prophet_df)
+# Forcasting into the future
+future = model1.make_future_dataframe(periods = 365)
+forecast = model1.predict(future)
+forecast
+figure = model1.plot(forecast,xlabel="Date",ylabel="Price")
+figure2 =model1.plot_components(forecast)
+# dataframes creation for both training and testing datasets
+avocado_df = pd.read_csv('avocado.csv')
+avocado_df.head()
+# Select specific region
+avocado_df_sample = avocado_df[avocado_df['region']=='West']
+avocado_df_sample=avocado_df_sample.sort_values('Date')
+plt.plot(avocado_df_sample['Date'],avocado_df_sample['AveragePrice'])
+avocado_df_sample =avocado_df_sample.rename(columns={'Date':'ds','AveragePrice':'y'})
+avocado_df_sample.head()
+m = Prophet()
+m.fit(avocado_df_sample)
+# Forcasting into the future
+future = m.make_future_dataframe(periods=365)
+forecast = m.predict(future)
+figure = m.plot(forecast, xlabel='Date', ylabel='Price')
+figure3 = m.plot_components(forecast)
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Prediction-of-Future-product-prices
+Predicting Future Product Prices using Facebook Prophet
+The future prices of avocados was predicted in this project with Facebook Prophet where Prophet is an opensource tool which is used for series forecasting
+The expected outcome was produced.
+
+
+Dataset explaination
+
+Date: The date of the observation
+AveragePrice: the average price of a single avocado
+type: conventional or organic
+year: the year
+Region: the city or region of the observation
+Total Volume: Total number of avocados sold
+4046: Total number of avocados with PLU 4046 sold
+4225: Total number of avocados with PLU 4225 sold
+4770: Total number of avocados with PLU 4770 sold
+
+About Facebook Prophet
+opensource tool developed by Facebook core DataScience team. It is a procedure for forecasting time series data based on an additive model where non-linear trends are fit with yearly,weekly and daily seasonality.It can be used for prediction of stock prices , product sales etc
+For additional information refer :
+https://research.fb.com/prophet-forecasting-at-scale/
+https://facebook.github.io/prophet/docs/quick_start.html
+
